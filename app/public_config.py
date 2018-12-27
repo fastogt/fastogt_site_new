2295e0f972cdaefbd1ac29c6c01e48167061dbf7
topilski <atopilski@gmail.com>
2018-12-27 06:13:07 +0300

    New tips

--- app/public_config.py
+++ app/public_config.py
@@ -1,5 +1,5 @@
 PUBLIC_CONFIG = {'site': {'title': 'FastoGT', 'keywords': 'opensource,outsource,outstaff'},
-                 'company': {'title': 'FastoGT', 'domain': 'http://fastogt.com'},
+                 'company': {'title': 'FastoGT', 'domain': 'https://fastogt.com'},
                  'support': {'contact_email': 'support@fastogt.com',
                              'contact_address': 'Republic of Belarus, Minsk, Stadionnay str. 5'},
                  'project': {'version': '1.0.0', 'version_type': 'release'}}

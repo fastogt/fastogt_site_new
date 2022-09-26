e324e85e89092d8823b092fb0e0e3ef81ce13b3c
Alexandr Topilski <atopilski@gmail.com>
2022-09-26 08:33:33 +0300

    Update public_config.py

--- app/public_config.py
+++ app/public_config.py
@@ -1,5 +1,5 @@
 PUBLIC_CONFIG = {'site': {'title': 'FastoGT', 'keywords': 'opensource,outsource,outstaff'},
                  'company': {'title': 'FastoGT', 'domain': 'https://fastogt.com'},
                  'support': {'contact_email': 'support@fastogt.com',
-                             'contact_address': 'Republic of Belarus, Minsk, Stadionnay str. 5'},
+                             'contact_address': 'Poland, Warszawa, Marcina Kasprzaka 25, 01-224'},
                  'project': {'version': '1.0.0', 'version_type': 'release'}}
